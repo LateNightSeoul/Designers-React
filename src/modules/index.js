79dd19a6188e53261fa4db9f39f9aab9af486722
LateNightCoding <leehae8@naver.com>
2021-03-11 22:13:06 +0900

    x

---
+++ src/modules/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import scheduler from './scheduler';
+
+const rootReducer = combineReducers({
+    scheduler
+});
+
+export default rootReducer;
---
+++ src/modules/scheduler.js
@@ -0,0 +1,34 @@
+const ADD_SCHEDULE = 'scheduler/ADD_SCHEDULE';
+const DELETE_SCHEDULE = 'scheduler/DELETE_SCHEDULE';
+
+let nextId = 1;
+export const addSchedule = (member_name, treatment_type, start_time, end_time) => ({
+    type: ADD_SCHEDULE,
+    schedule: {
+        id: nextId++,
+        member_name,
+        treatment_type,
+        start_time,
+        end_time
+    }
+});
+
+
+const initialState = [
+    {
+        id: 1,
+        member_name: '이해석',
+        treatment_type: '펌',
+        start_time: '2021-03-14 13:00',
+        end_time: '2021-03-14 14:00'
+    }
+]
+
+export default function schedule(state = initialState, action) {
+    switch(action.type) {
+        case ADD_SCHEDULE:
+            return state.concat(action.schedule);
+        default:
+            return state;
+    }
+}
