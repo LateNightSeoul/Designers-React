1e1abc6fee5d7ab9235c08161dac4a1b09ad333f
LateNightCoding <leehae8@naver.com>
2021-03-12 22:17:57 +0900

    feat : 스케줄러 헤더의 요일 및 일 출력 기능 구현

--- src/js/View/SchedulerHeader.js
+++ src/js/View/SchedulerHeader.js
@@ -1,28 +1,53 @@
 import React from 'react';
 
-function HeaderComponent( {date} ) {
-    const days_of_week = ['일', '월', '화', '수', '목', '금', '토'];
-
+function HeaderComponent( { date, day } ) {
     return (
         <td>
-            <div>{days_of_week[date.day]}</div>
-            <div>{date.date}</div>
+            <div>{date}</div>
+            <div>{day}</div>
         </td>
     )
 }
 
-function SchedulerHeader( {date} ) {
+function SchedulerHeader( {} ) {
     const dateObj = new Date();
+    const days_of_week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
 
-    date = {
+    const date = {
         year: dateObj.getFullYear(),
         month: dateObj.getMonth(),
         date: dateObj.getDate(),
         day: dateObj.getDay()
     }
 
-    const 
+    function getDays() {
+        let days = [];
+        let idx = date.day;
+        for (let i = 0; i < 7; i++) {
+            days.push(days_of_week[idx % 7]);
+            idx++;
+        }
+        return days
+    }
 
+    function getDates() {
+        let dates = [];
+        const dateObj = new Date();
+        for(let i = 0; i < 7; i++) {
+            dateObj.setDate(dateObj.getDate() + 1);
+            dates.push(dateObj.getDate());
+        }
+        return dates;
+    }
+
+    const days = getDays();
+    const dates = getDates();
+
+    // const dates = getDate(dateObj);
+    // const printHeaderComponent = days.map(
+    //     (day, idx) => (<HeaderComponent key={idx} date={dates[idx]} day={day}/>)
+    // )
+    
     return (
         <tr>
         <td>
@@ -33,7 +58,8 @@ function SchedulerHeader( {date} ) {
           <div>분</div>
           <div></div>
         </td>
-        <HeaderComponent date={date}/>
+        {days.map((day, idx) => (
+            <HeaderComponent key={idx} date={dates[idx]} day={day}/>))}
       </tr>
     )
 }
