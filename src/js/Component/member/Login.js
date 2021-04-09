e08e9c5fda5eb8c67066b10d61b9a313fc1ddd72
LateNightCoding <leehae8@naver.com>
2021-04-09 22:32:55 +0900

    feat : 로그인 및 회원가입 구현

--- src/js/Component/member/Login.js
+++ src/js/Component/member/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './css/login.css';
+import axios from 'axios';
 
 function Login() {
 
@@ -8,14 +9,14 @@ function Login() {
         password: "",
     })
 
-    const handleId = e => {
+    const onChangeId = e => {
         setLoginInfo({
             ...loginInfo,
             id: e.target.value
         })
     }
 
-    const handlePassword = e => {
+    const onChangePassword = e => {
         setLoginInfo({
             ...loginInfo,
             password: e.target.value
@@ -30,17 +31,11 @@ function Login() {
             return
         } 
 
-        const login_info = {
-            method: "POST",
-            body: JSON.stringify(loginInfo),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-        fetch("http://localhost:8080/login", login_info)
-        .then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error));
+        const url = 'http://localhost:8080/login';
+
+        axios.post(url, loginInfo)  
+        .then((res) => { console.log(res.data) })
+        .catch((res) => { console.log('id 혹은 비밀번호를 확인하세요.');})
     }
 
     return (
@@ -54,13 +49,13 @@ function Login() {
                 type={Text} 
                 id={'login-id'} 
                 placeholder={'ID'}
-                onChange={handleId}/>
+                onChange={onChangeId}/>
             <input 
                 type={Text} 
                 id={'login-password'} 
                 placeholder={'PASSWORD'}
                 type='password'
-                onChange={handlePassword}/>
+                onChange={onChangePassword}/>
 
 
             <button type='submit'>로그인</button>
--- src/js/Component/member/SignUp.js
+++ src/js/Component/member/SignUp.js
@@ -1,35 +1,72 @@
 import React, { useState } from 'react';
 import './css/signup.css';
+import axios from 'axios';
 
 function SignUp() {
     const [signupInfo, setSignupInfo] = useState({
         name: "",
         sex: "",
-        email: "",
+        email_id: "",
+        email_address: "",
         id: "",
+        id_verify: false,
         password: "",
         password_confirm: "",
-        phoneNumber: "",
+        phone_number: "",
     })
 
-    const checkSignupInfo = () => {
+    const onChangeInfo = e => {
+        setSignupInfo({
+            ...signupInfo,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const checkEmpty = () => {
         for (const [key, value] of Object.entries(signupInfo)) {
             if (value === "") {
                 alert("정보를 모두 입력하세요.");
-                return
+                return false
             }
         }
+        return true
+    }
 
+    const checkPassword = () => {
         if (signupInfo.password != signupInfo.password_confirm) {
             alert("비밀번호가 일치하지 않습니다.");
+            return false
+        }
+        return true
+    }
+
+    const checkSignupInfo = () => {
+        if(checkEmpty() === false) {
             return
         }
+        
+        if(checkPassword() === false) {
+            return
+        }
+    }
 
+    const handleIdVerify = e => {
+        e.preventDefault();
+        
+        const url = "http://localhost:8080/signup/idverify";
+
+        axios.post(url, signupInfo.id)
+        .then((res) => { console.log(res.status) })
+        .catch((res) => { console.log('이미 존재하는 id입니다.'); } )
         
     }
 
     const handleSubmit = e => {
         e.preventDefault();
+
+        checkSignupInfo();
+
+        const url = "http://localhost:8080/signup";
         
         const signup_info_dto = {
             name: signupInfo.name,
@@ -39,37 +76,64 @@ function SignUp() {
             password: signupInfo.password,
             phoneNumber: signupInfo.phoneNumber,
         }
-        
-        const signup_info = {
-            method: "POST",
-            body: JSON.stringify(signup_info_dto),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-        fetch("http://localhost:8080/login", signup_info)
-        .then(res => res.json())
-        .then(response => console.log('Success:', JSON.stringify(response)))
-        .catch(error => console.error('Error:', error));
+    
+        axios.post(url, signup_info_dto)
+        .then((res) => { console.log(res.status) })
+        .catch((res) => { console.log('Error'); } )
     }
-
 
     return (
         <div className={'signup-container'}>
             <form onSubmit={handleSubmit}>
                 <div className={'signup-logo'}>회원가입</div>
                 <div>
-                    <input type={Text} placeholder={'이름'}/>
-                    <input type={Text} placeholder={'성별'}/>
-                    <input type={Text} placeholder={'이메일'}/>
+                    <input 
+                        type={Text} 
+                        placeholder={'이름'}
+                        name='name'
+                        onChange={onChangeInfo}/>
+                    <select
+                        type={Text} 
+                        placeholder={'성별'}
+                        name='sex'
+                        onChange={onChangeInfo}>
+                            <option></option>
+                            <option value="male">남자</option>
+                            <option value="female">여자</option>
+                    </select>
+                    <input 
+                        type={Text} 
+                        placeholder={'이메일'}
+                        name='email_id'
+                        onChange={onChangeInfo}/>
                     <span>@</span>
-                    <input type={Text}/>
-                    <input type={Text} placeholder={'아이디'}/>
+                    <input 
+                        type={Text}
+                        name='email_address'
+                        onChange={onChangeInfo}/>
+                    <input 
+                        type={Text} 
+                        placeholder={'아이디'}
+                        name='id'
+                        onChange={onChangeInfo}/>
                     
-                    <button>중복확인</button>
-                    <input type={Text} placeholder={'비밀번호'}/>
-                    <input type={Text} placeholder={'비밀번호 확인'}/>
-                    <input type={Text} placeholder={'휴대폰인증'}/>
+                    <button type='button' onClick={handleIdVerify}>중복확인</button>
+
+                    <input 
+                        type={Text} 
+                        placeholder={'비밀번호'}
+                        name='password'
+                        onChange={onChangeInfo}/>
+                    <input 
+                        type={Text} 
+                        placeholder={'비밀번호 확인'}
+                        name='password_confirm'
+                        onChange={onChangeInfo}/>
+                    <input 
+                        type={Text} 
+                        placeholder={'휴대폰인증'}
+                        name='phone_number'
+                        onChange={onChangeInfo}/>
                 </div>
                 <div>
                     <button type='submit'>가입</button>
