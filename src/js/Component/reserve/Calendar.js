dbe633d75a620815638484537899ad67ecce6060
LateNightCoding <leehae8@naver.com>
2021-04-18 22:58:23 +0900

    feat: 데이터에 따라 가능한 날, 불가능한 날 달력에 표시하는 기능 구현

--- src/js/Component/reserve/Calendar.js
+++ src/js/Component/reserve/Calendar.js
@@ -1,49 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-function DateComponent({ first_date, last_date }) {
+function DateComponent({ today, first_date, last_date, selectedDate, setSelectedDate }) {
 
     const mockData = [
         {
-            date: 18, // 18일은 꽉 차있는 스케줄로써 달력 클릭 불가하도록 설정할 것
-            schedule : [
-                {
-                    start_time: '12:00',
-                    end_time: '14:00',
-                    blank_time_start: '13:00',
-                    blank_time_end: '13:30'
-                },
-                {
-                    start_time: '13:00',
-                    end_time: '13:30',
-                    blank_time_start: '',
-                    blank_time_end: ''
-                },
-                {
-                    start_time: '14:00',
-                    end_time: '20:00',
-                    blank_time_start: '',
-                    blank_time_end: ''
-                },
-            ]
+            date: 22,
+            available: false
         },
         {
-            date: 21,
-            schedule : [
-                {
-                    start_time: '12:00',
-                    end_time: '14:00',
-                    blank_time_start: '13:00',
-                    blank_time_end: '13:30'
-                }
-            ]
+            date: 28,
+            available: false
         }
     ]
 
     const [calendar, setCalendar] = useState({});
 
-    const isAvailable = () => {
-        
+    const isAvailable = (date) => {
+        const date_style = {
+            color: 'red',
+            cursor: 'pointer',
+        }
+
+        if(date >= today.getDate()) {
+            for (let i = 0; i < mockData.length; i++) {
+                if(mockData[i].date == date && mockData[i].available === false) {
+                    return <td>{date}</td>
+                }
+            }
+            return <td style={date_style}>{date}</td>
+        } else {
+            return <td>{date}</td>
+        }
     }
 
     const getCalendarData = () => {
@@ -51,7 +39,7 @@ function DateComponent({ first_date, last_date }) {
         const last_date_of_full_calendar = 42;
         for (let i = 0; i < last_date_of_full_calendar; i++) {
             if(i >= first_date.getDay() && i < (last_date.getDate() + first_date.getDay())) {
-                calnedar_data.push(<td>{i - first_date.getDay() + 1}</td>);
+                calnedar_data.push(isAvailable(i - first_date.getDay() + 1));
             } else {
                 calnedar_data.push(<td></td>);
             }
@@ -60,6 +48,7 @@ function DateComponent({ first_date, last_date }) {
     }
 
     const calnedar_data = getCalendarData();
+    console.log(calnedar_data);
 
     const row_numbers = [0,1,2,3,4,5];
     const column_numbers = [0,1,2,3,4,5,6];
@@ -82,6 +71,8 @@ function Calendar() {
         first_date: new Date(today.getFullYear(), today.getMonth(), 1),
         last_date: new Date(today.getFullYear(), today.getMonth() + 1, 0)
     })
+
+    const [selectedDate, setSelectedDate] = useState(0);
 
     const onClickNext = () => {
         let next_date = new Date(date.today.getFullYear(), date.today.getMonth() + 1);
@@ -106,13 +97,18 @@ function Calendar() {
             <div>달력</div>
             <div>
                 <span>{date.today.getFullYear()}년</span>
-                <span>{date.today.getMonth()}월</span>
+                <span>{date.today.getMonth()+1}월</span>
             </div>
             <table>
                 <tr>
                     {days_of_week.map((day, i) => (<td>{day}</td>))}
                 </tr>
-                <DateComponent first_date={date.first_date} last_date={date.last_date}/>
+                <DateComponent 
+                    first_date={date.first_date} 
+                    last_date={date.last_date} 
+                    today={today} 
+                    selectedDate={selectedDate}
+                    setSelectedDate={setSelectedDate}/>
             </table>
             <button onClick={onClickPrev}>이전</button>
             <button onClick={onClickNext}>다음</button>
