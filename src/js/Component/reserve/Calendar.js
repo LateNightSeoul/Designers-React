f24bf9b248f7c4add04ac7b25e90b8fb90c874f9
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2021-04-19 06:11:03 +0900

    feat : 날짜 선택 기능 구현

--- src/js/Component/reserve/Calendar.js
+++ src/js/Component/reserve/Calendar.js
@@ -1,7 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import './calendar.css';
 
-function DateComponent({ today, first_date, last_date, selectedDate, setSelectedDate }) {
+function DateComponent({calendar_data, row, column, mockData, today, selectedDate, setSelectedDate }) {
+
+    const date = calendar_data[row * 7 + column];
+
+    const isAvailable = (date) => {
+        if(date >= today.getDate()) {
+            for (let i = 0; i < mockData.length; i++) {
+                if(mockData[i].date == date && mockData[i].available === false) {
+                    return false
+                }
+            }
+            return true
+        } else {
+            return false
+        }
+    }
+
+    const isSelected = () => {
+        if(isAvailable(date) === false) {
+            return false
+        }
+        if(row == selectedDate.row && column == selectedDate.column){
+            return true
+        } 
+        return false
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        if(isAvailable(date) === false) {
+            return
+        }
+        setSelectedDate({row : row, column : column});
+        console.log(selectedDate);
+    }
+
+    return (
+        <td className={[(isAvailable(date) === true && 'is-active'),
+                        (isSelected() === true && 'is-selected')].join(' ')}
+            onClick={handleClick}>
+                {date === 0 ? '' : date}
+        </td>
+    )
+}
+
+function RowComponent({ calendar_data, row, mockData, today, setSelectedDate, selectedDate }) {
+    const column_numbers = [0,1,2,3,4,5,6];
+
+    console.log(calendar_data);
+
+    return (
+        <tr>{column_numbers.map((column, i) => (<DateComponent 
+                                                    calendar_data={calendar_data} 
+                                                    row={row} 
+                                                    column={column} 
+                                                    mockData={mockData} 
+                                                    today={today} 
+                                                    setSelectedDate={setSelectedDate}
+                                                    selectedDate={selectedDate}/>))}</tr>
+    )
+}
+
+function MainComponent({ today, first_date, last_date }) {
+
+    const [selectedDate, setSelectedDate] = useState(0);
 
     const mockData = [
         {
@@ -14,49 +79,38 @@ function DateComponent({ today, first_date, last_date, selectedDate, setSelected
         }
     ]
 
-    const [calendar, setCalendar] = useState({});
-
-    const isAvailable = (date) => {
-        const date_style = {
-            color: 'red',
-            cursor: 'pointer',
-        }
-
-        if(date >= today.getDate()) {
-            for (let i = 0; i < mockData.length; i++) {
-                if(mockData[i].date == date && mockData[i].available === false) {
-                    return <td>{date}</td>
-                }
-            }
-            return <td style={date_style}>{date}</td>
-        } else {
-            return <td>{date}</td>
-        }
+    const handleClick = (date) => {
+        
     }
 
-    const getCalendarData = () => {
-        let calnedar_data = [];
+    const initCalendar = () => {
+        let calendar_data = [];
         const last_date_of_full_calendar = 42;
         for (let i = 0; i < last_date_of_full_calendar; i++) {
             if(i >= first_date.getDay() && i < (last_date.getDate() + first_date.getDay())) {
-                calnedar_data.push(isAvailable(i - first_date.getDay() + 1));
+                calendar_data.push(i - first_date.getDay() + 1);
             } else {
-                calnedar_data.push(<td></td>);
+                calendar_data.push(0);
             }
         }
-        return calnedar_data;
+        return calendar_data;
     }
 
-    const calnedar_data = getCalendarData();
-    console.log(calnedar_data);
+    const calendar_data = initCalendar();
+
+    console.log(calendar_data);
 
     const row_numbers = [0,1,2,3,4,5];
-    const column_numbers = [0,1,2,3,4,5,6];
-
+    
     return(
         <React.Fragment>
             {row_numbers.map((row) => {
-                return <tr>{column_numbers.map((column) => (calnedar_data[row*7 + column]))}</tr>
+                return <RowComponent 
+                            calendar_data={calendar_data} 
+                            row={row} mockData={mockData} 
+                            today={today}
+                            setSelectedDate={setSelectedDate}
+                            selectedDate={selectedDate}/>
             })}
         </React.Fragment>
     )
@@ -103,12 +157,11 @@ function Calendar() {
                 <tr>
                     {days_of_week.map((day, i) => (<td>{day}</td>))}
                 </tr>
-                <DateComponent 
+                <MainComponent 
                     first_date={date.first_date} 
                     last_date={date.last_date} 
                     today={today} 
-                    selectedDate={selectedDate}
-                    setSelectedDate={setSelectedDate}/>
+                    />
             </table>
             <button onClick={onClickPrev}>이전</button>
             <button onClick={onClickNext}>다음</button>
