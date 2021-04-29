0a1cfbfedd1e1cae7ae20765d7573368f147b328
jylee9506 <69140464+jylee9506@users.noreply.github.com>
2021-04-29 19:57:28 +0900

    ㅁㅇㄻㄷㄹ

---
+++ src/js/Component/Example.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Sub() {
+    return(
+        <div>
+            안녕 난 해석이야
+        </div>
+    )
+}
+
+function LeeJongYoung() {
+    return (
+        <div>
+            <h1> 종영 좋아 ㅎㅎ </h1>
+            <Sub></Sub>
+        </div>
+    )
+}
+
+export default LeeJongYoung;
